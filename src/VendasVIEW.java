674bcb3acd79c8caca1e453d1f7116cced79bf38
pedrombas <pedrohmangueiraa@gmail.com>
2026-05-24 23:19:45 -0300

    implementada navegacao para tela de vendas

--- src/VendasVIEW.java
+++ src/VendasVIEW.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
-/**
- *
- * @author pedro
- */
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -6,15 +6,6 @@ import javax.swing.JOptionPane;
 
 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template a a ad
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
     public Connection connectDB(){
